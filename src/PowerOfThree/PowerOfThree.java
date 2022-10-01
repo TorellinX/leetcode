25e47f6208e2d9f1243c217b4f2aa4d9e0ff14fd
Morozova Alena <morozovaalenay@gmail.com>
2022-10-01 12:39:59 +0200

    326. Power of Three

---
+++ src/PowerOfThree/PowerOfThree.java
@@ -0,0 +1,50 @@
+/*
+326. Power of Three
+Easy
+
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+Example 1:
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+
+Example 2:
+Input: n = 0
+Output: false
+Explanation: There is no x where 3x = 0.
+
+Example 3:
+Input: n = -1
+Output: false
+Explanation: There is no x where 3x = (-1).
+
+Constraints:
+-231 <= n <= 231 - 1
+
+Follow up: Could you solve it without loops/recursion?
+ */
+package PowerOfThree;
+
+public class PowerOfThree {
+
+  public boolean isPowerOfThree1(int n) {
+    if (n <= 0) {
+      return false;
+    }
+    while (n % 3 == 0) {
+      n /= 3;
+    }
+    return n == 1;
+  }
+
+  public boolean isPowerOfThree2(int n) {
+    if (n <= 0) {
+      return false;
+    }
+    double epsilon = 0.0000000001;
+    return (Math.log10(n) / Math.log10(3) + epsilon) % 1 <= 2 * epsilon;
+  }
+
+}
